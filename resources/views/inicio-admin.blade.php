d16324333fc496e8d4444410830355eaafb1fe41
nosoymilton <miltonbarra86@gmail.com>
2019-07-01 22:52:16 -0400

    01-07-2019 milton

--- resources/views/inicio-admin.blade.php
+++ resources/views/inicio-admin.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     @section('parte1')
-    <h1 class="w3-text-teal embed-responsive-item">Bienvenido a la Biblioteca de la agrupacion abanico</h1>
+    <h1 class="w3-text-teal embed-responsive-item">Bienvenido a la Biblioteca de la agrupacion abanico Milton</h1>
     <div class="embed-responsive embed-responsive-16by9">
         <iframe src="https://www.youtube.com/embed/LSlGfZfM3Us" frameborder="0" all="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
